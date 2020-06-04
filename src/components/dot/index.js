d9fe6cb0d64d59350eb27fe27e45fae3fc8fa7eb
lukas1013 <leonardolukas10@gmail.com>
2020-06-04 13:03:57 -0300

    game width and height added

--- src/components/dot/index.js
+++ src/components/dot/index.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+import './styles.css';
+
 export default function Dot({posX, vel = 1}) {
 	const [posY, setY] = useState(0);
 	
 	useEffect(() => {
 		
-	});
+	}, []);
 	
 	return (
-		<rect x={posX} y={posY} width='1' height='1'/>
+		<rect x={posX}
+			y={posY}
+			width='10'
+			height='10'
+			fill='darkgray'
+			className='dot'/>
 	);
 }
