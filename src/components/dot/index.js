07326cd442fd2fb047aaa81efb98be0f3e3936c3
lukas1013 <leonardolukas10@gmail.com>
2020-06-04 18:14:35 -0300

    point component added

--- src/components/dot/index.js
+++ src/components/dot/index.js
@@ -6,15 +6,22 @@ export default function Dot({posX, vel = 1}) {
 	const [posY, setY] = useState(0);
 	
 	useEffect(() => {
+		let timer;
 		
-	}, []);
+		if (posY === 190) 
+			return clearTimeout(timer);
+			
+		timer = setTimeout(() => {
+			setY(posY + 10)
+		}, 1500 * vel);
+	}, [posY]);
 	
 	return (
 		<rect x={posX}
 			y={posY}
 			width='10'
 			height='10'
-			fill='darkgray'
+			fill='white'
 			className='dot'/>
 	);
 }
