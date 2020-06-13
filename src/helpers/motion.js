5e168821556e86934778a66caa207dac96b845d6
lukas1013 <leonardolukas10@gmail.com>
2020-06-12 22:06:11 -0300

    fixex canMoveLeft function

--- src/helpers/motion.js
+++ src/helpers/motion.js
@@ -1,34 +1,13 @@
 import gameConfig from '../config/game';
 
-export function canMoveLeft(poliminos, polimino) {
-	if (polimino.hasArrived || polimino.posX === gameConfig.minX)
-			return false;
-			
-	if (poliminos.some(p => {
-		return p.posX === polimino.posX - 10 && p.posY === polimino.posY
-	})) {
-		return false;
-	}
-	return true;
-}
-
-export function canMoveRight(poliminos, polimino) {
-	if (polimino.hasArrived || polimino.posX === gameConfig.maxX)
-			return false;
-			
-	if (poliminos.some(p => {
-		return p.posX === polimino.posX + 10 && p.posY === polimino.posY
-	})) {
-		return false;
-	}
-	return true;
-}
-
 export function canFall(poliminos, polimino) {
+	if (polimino.hasArrived)
+		return false
+	
 	let points;
 	
+	//extracting the coords from the collision points
 	if (polimino.type === 't') {
-		//extracting the coords from the collision points
 		const [c0,,c2,c3] = polimino.coords;
 		points = [c0,c2,c3];
 	}
@@ -51,4 +30,51 @@ export function canFall(poliminos, polimino) {
 	
 	//if collided, can't fall
 	return !collided;
+}
+
+export function canMoveLeft(poliminos, polimino) {
+	if (polimino.hasArrived)
+		return false
+	
+	let points;
+	
+	if (polimino.type === 't') {
+		const [c0,,,c3] = polimino.coords;
+		points = [c0,c3];
+	}
+	
+	const collided = points.some(point => {
+		//hit the left side
+		if (point.x === gameConfig.minX)
+			return point
+		
+		if(poliminos.some(p => {
+			const { hasArrived, coords} = p;
+			if (coords.some(c => {
+				//ignores the same array
+				if (coords === polimino.coords)
+					return false;
+				//alert(`${c.x === point.x - 10}: point: ${JSON.stringify(point)}\ncoord: ${JSON.stringify(c)}`)
+				return c.x === point.x - 10 && c.y === point.y
+			})) {
+				return p
+			}
+		})) {
+			return point
+		}
+	});
+	
+	return !collided;
+}
+
+export function canMoveRight(poliminos, polimino) {
+	if (polimino.hasArrived || polimino.posX === gameConfig.maxX)
+			return false;
+			
+	if (poliminos.some(p => {
+		return p.posX === polimino.posX + 10 && p.posY === polimino.posY
+	})) {
+		return false;
+	}
+	return true;
 }
