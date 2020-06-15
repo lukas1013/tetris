6478dff015690ca003451b7a3139619ffb2599bd
lukas1013 <leonardolukas10@gmail.com>
2020-06-15 09:51:05 -0300

    added polimino I movement

--- src/helpers/motion/index.js
+++ src/helpers/motion/index.js
@@ -1,5 +1,6 @@
 import * as t from './t';
 import * as o from './o';
+import * as i from './i';
 
 export function canFall(poliminos, polimino) {
 	if (polimino.hasArrived)
@@ -9,6 +10,8 @@ export function canFall(poliminos, polimino) {
 		return t.canFall(poliminos, polimino)
 	if (polimino.type === 'o')
 		return o.canFall(poliminos, polimino)
+	if (polimino.type === 'i')
+		return i.canFall(poliminos, polimino)
 }
 
 export function canMoveLeft(poliminos, polimino) {
@@ -19,6 +22,8 @@ export function canMoveLeft(poliminos, polimino) {
 		return t.canMoveLeft(poliminos, polimino)
 	if (polimino.type === 'o')
 		return o.canMoveLeft(poliminos, polimino)
+	if (polimino.type === 'i')
+		return i.canMoveLeft(poliminos, polimino)
 }
 
 export function canMoveRight(poliminos, polimino) {
@@ -29,4 +34,6 @@ export function canMoveRight(poliminos, polimino) {
 		return t.canMoveRight(poliminos, polimino)
 	if (polimino.type === 'o')
 		return o.canMoveRight(poliminos, polimino)
+	if (polimino.type === 'i')
+		return i.canMoveRight(poliminos, polimino)
 }
